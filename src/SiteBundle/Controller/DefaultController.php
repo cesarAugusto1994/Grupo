3b3c91461079ee17cb91f69525b6645ab97d8c03
Cesar Augusto Sousa <cezzaar@gmail.com>
2016-06-14 08:44:51 -0300

    primeiro commit

---
+++ src/SiteBundle/Controller/DefaultController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('SiteBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/ensaios", name="ensaios")
+     */
+    public function ensaiosAction()
+    {
+        // replace this example code with whatever you need
+        return $this->render('@Site/Default/ensaios.html.twig');
+    }
+
+    /**
+     * @Route("/contato", name="contato")
+     */
+    public function contatoAction()
+    {
+        // replace this example code with whatever you need
+        return $this->render('@Site/Default/contato.html.twig');
+    }
+}
